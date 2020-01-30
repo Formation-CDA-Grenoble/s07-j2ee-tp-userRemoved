07fc689cca58f6b4a72c6ee7de2afc5bc7ccbd17
Romain Tello <44443864+Moonpearl@users.noreply.github.com>
2020-01-30 09:24:24 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Travaux pratiques Spring
+
+Travaux pratiques pour s'entraîner à la création d'applications MVC avec Spring.
---
+++ src/test/java/com/example/s07j2eetp/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.s07j2eetp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
